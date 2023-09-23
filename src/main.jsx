4fa5bb69d7ebb53f4633f7401b5d4c5cb50422c5
WhitneySt <whitneystena418@gmail.com>
2023-09-22 21:09:12 -0500

    Creando el repositorio del ejercicio en clase Front �‍�

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Router from './routes/router'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+ <Router/>
+)
---
+++ src/services/endpoints.js
@@ -0,0 +1,7 @@
+const URL_BASE = 'https://miniback-books-app.onrender.com/';
+
+const endpoints = {
+    library: `${URL_BASE}library`
+}
+
+export default endpoints;

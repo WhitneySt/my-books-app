c1c314c4b3c4ab920df3c113ff59f47fe9a685b7
WhitneySt <whitneystena418@gmail.com>
2023-09-26 21:34:52 -0500

    Finalizando funcionalidad de filtrado por cantidad de páginas y género

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -5,11 +5,13 @@ import './home.scss';
 
 const Home = () => {
     const [books, setBooks] = useState([]);
+    const [filterResult, setFilterResult] = useState([]);
+    const [responseFilter, setResponseFilter] = useState('');
     const [categories, setCategories] = useState([]);
     const [rangePages, setRangePages] = useState({
         min: 0,
         max: 1000,
-        step:10
+        step: 10
     });
     const [filters, setFilters] = useState({})
 
@@ -18,7 +20,7 @@ const Home = () => {
             setBooks(response);
             console.log(response)
             const categoriesLis = getCategories(response);
-            const range=getPages(response);
+            const range = getPages(response);
             setCategories(categoriesLis);
             setRangePages({
                 ...rangePages,
@@ -44,15 +46,33 @@ const Home = () => {
     }
 
     const onFilter = (event) => {
-        const { name, value } = event['target'];        
-        setFilters({
-            ...filters,
-            [name]:value
-        })
-        console.log({
+        const { name, value } = event.target;
+        const filterParams = {
             ...filters,
             [name]: value
-        })
+        }
+        setFilters(filterParams)
+        // console.log({
+        //     ...filters,
+        //     [name]: value
+        // })
+        if (value) {
+            let filtered = [...books];
+            for (const key in filterParams) {
+                if (filterParams[key]) {
+                    const filteredResult = key === 'pages' ? filtered.filter(element => element.book[key] <= filterParams[key]) : filtered.filter((element) => element.book[key] == filterParams[key])
+                    filtered = [...filteredResult];
+                }
+            }
+            // console.log(filtered);
+            setFilterResult(filtered);
+            setResponseFilter(()=>filtered.length?'': 'No se encontraron resultados')
+        } else {
+            setFilterResult([]);
+            setResponseFilter('Filtros limpiados')
+        }
+
+
     }
 
     return (
@@ -60,7 +80,7 @@ const Home = () => {
             <section className='filtersContainer'>
                 <div>
                     <label>Filtrar por páginas</label>
-                    <input type="range" min={rangePages.min} max={rangePages.max} step={rangePages.step} onChange={onFilter} name="pages" value={filters.pages}/>
+                    <input type="range" min={rangePages.min} max={rangePages.max} step={rangePages.step} onChange={onFilter} name="pages" value={filters.pages} />
                 </div>
                 <div>
                     <label>Filtrar por género</label>
@@ -72,8 +92,14 @@ const Home = () => {
                     </select>
                 </div>
             </section>
+            {
+                responseFilter && <h2>{responseFilter}</h2>
+            }
             <section className='cardsContainer'>
                 {
+                    filterResult.length ? filterResult.map((item, index) => <figure key={index}>
+                        <img src={item.book.cover} alt={item.book.title} />
+                    </figure>):
                     books.length > 0 ? books.map((item, index) => <figure key={index}>
                         <img src={item.book.cover} alt={item.book.title} />
                     </figure>) : <div>...Cargando</div>
